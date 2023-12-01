03fc6ccd1f7262304d3b798147c77e83563af408
Tch1b0 <Jpour3006@gmail.com>
2023-12-01 14:50:23 +0100

    finish day 1

---
+++ days/01/README.md
@@ -0,0 +1,3 @@
+# AOC day 01
+
+[Task](https://adventofcode.com/2022/day/1)
---
+++ days/01/solution.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	stdstrings "strings"
+
+	"github.com/Tch1b0/polaris/input"
+	"github.com/Tch1b0/polaris/strings"
+)
+
+var digit = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func getInput() []string {
+	return input.Process("./days/01/input.txt", func(str string) []string {
+		return stdstrings.Split(str, "\n")
+	})
+}
+
+func getNumbers(str string) []int {
+	nums := []int{}
+	for _, c := range str {
+		if c >= '0' && c <= '9' {
+			nums = append(nums, int(c-'0'))
+		}
+	}
+
+	return nums
+}
+
+func part1(in []string) int {
+	sum := 0
+	for _, line := range in {
+		nums := getNumbers(line)
+		v, _ := strings.Atoi(fmt.Sprintf("%d%d", nums[0], nums[len(nums)-1]))
+		sum += v
+	}
+
+	return sum
+}
+
+func containsWordDigit(str string) *int {
+	for key, value := range digit {
+		if stdstrings.Contains(str, key) {
+			return &value
+		}
+	}
+
+	return nil
+}
+
+func getResultNum(str string) int {
+	var buf string
+	digits := ""
+
+	// first number
+	buf = ""
+	for _, c := range str {
+		buf += string(c)
+		if c >= '0' && c <= '9' {
+			digits += string(c)
+			break
+		} else if d := containsWordDigit(buf); d != nil {
+			digits += strings.Itoa(*d)
+			break
+		}
+	}
+
+	// last number
+	buf = ""
+	for i := len(str) - 1; i >= 0; i-- {
+		c := str[i]
+		buf = string(c) + buf
+		if c >= '0' && c <= '9' {
+			digits += string(c)
+			break
+		} else if d := containsWordDigit(buf); d != nil {
+			digits += strings.Itoa(*d)
+			break
+		}
+	}
+
+	res, _ := strings.Atoi(digits)
+	return res
+}
+
+func part2(in []string) int {
+	sum := 0
+	for _, line := range in {
+		v := getResultNum(line)
+		fmt.Println(v)
+		sum += v
+	}
+	return sum
+}
+
+func main() {
+	in := getInput()
+	// fmt.Printf("PART 1: %d\n", part1(in))
+
+	fmt.Printf("PART 2: %d\n", part2(in))
+}
