5267cc4fcfb624c3439463af646d78335f91e633
Tch1b0 <Jpour3006@gmail.com>
2023-12-03 09:51:06 +0100

    solve day 3 part 2

--- days/03/solution.go
+++ days/03/solution.go
@@ -4,120 +4,203 @@ import (
 	"fmt"
 	stdstrings "strings"
 
+	"github.com/Tch1b0/polaris/array"
 	"github.com/Tch1b0/polaris/input"
+	"github.com/Tch1b0/polaris/math"
 	"github.com/Tch1b0/polaris/runes"
 	"github.com/Tch1b0/polaris/strings"
 )
 
-var (
-	colorReset = "\033[0m"
+// shortcut for creating an int vector
+func vec(x, y int) math.Vector2[int] {
+	return math.Vector2[int]{X: x, Y: y}
+}
 
-	colorRed   = "\033[31m"
-	colorGreen = "\033[32m"
-)
+type Part struct {
+	Positions []math.Vector2[int]
+	value     string
+	IsSymbol  bool
+	IsNumber  bool
+}
 
-type Engine [][]rune
+func (p Part) Eq(o Part) bool {
+	return array.All(p.Positions, func(pos math.Vector2[int], i int) bool {
+		return pos == o.Positions[i]
+	})
+}
 
-func getInput() Engine {
-	return input.Process("./days/03/input.txt", func(str string) Engine {
-		lines := stdstrings.Split(stdstrings.ReplaceAll(str, "\r", ""), "\n")
-		e := Engine{}
+func (p Part) String() string {
+	return fmt.Sprintf("{%s at %s}", p.value, p.Positions[0].String())
+}
 
-		for _, line := range lines {
-			e = append(e, strings.ToRunes(line))
+type Engine struct {
+	Parts []Part
+}
+
+func (e Engine) atPos(pos math.Vector2[int]) *Part {
+	for _, part := range e.Parts {
+		for _, partPos := range part.Positions {
+			if partPos == pos {
+				return &part
+			}
+		}
+	}
+
+	return nil
+}
+
+func (e Engine) neighboursOf(p Part) []Part {
+	neighbourPositions := []math.Vector2[int]{}
+	for i, pos := range p.Positions {
+		neighbourPositions = append(neighbourPositions, pos.Add(vec(0, 1)))
+		neighbourPositions = append(neighbourPositions, pos.Sub(vec(0, 1)))
+
+		if i == 0 {
+			neighbourPositions = append(neighbourPositions, pos.Sub(vec(1, 0)))
+			neighbourPositions = append(neighbourPositions, pos.Sub(vec(1, 1)))
+			neighbourPositions = append(neighbourPositions, pos.Add(vec(-1, 1)))
 		}
 
-		return e
-	})
+		if i == len(p.Positions)-1 {
+			neighbourPositions = append(neighbourPositions, pos.Add(vec(1, 0)))
+			neighbourPositions = append(neighbourPositions, pos.Add(vec(1, 1)))
+			neighbourPositions = append(neighbourPositions, pos.Add(vec(1, -1)))
+		}
+	}
+
+	neighbours := []Part{}
+	for _, np := range neighbourPositions {
+		n := e.atPos(np)
+		// check if neighbour exists, and isn't already being tracked
+		if n != nil && array.None(neighbours, func(v Part, _ int) bool { return v.Eq(*n) }) {
+			neighbours = append(neighbours, *n)
+		}
+	}
+
+	return neighbours
 }
 
 func isSymbol(r rune) bool {
 	return !runes.IsDigit(r) && r != '.'
 }
 
-func hasSymbolNeighbours(e Engine, x, y int) bool {
-	xIsBottom := x == 0
-	xIsTop := x == len(e[y])-1
+func getInput() Engine {
+	return input.Process("./days/03/input.txt", func(str string) Engine {
+		lines := stdstrings.Split(stdstrings.ReplaceAll(str, "\r", ""), "\n")
+		e := Engine{Parts: []Part{}}
 
-	yIsBottom := y == 0
-	yIsTop := y == len(e)-1
+		for y, line := range lines {
+			buf := ""
+			bufPositions := []math.Vector2[int]{}
 
-	return (!xIsBottom && isSymbol(e[y][x-1])) || // check left
-		(!xIsTop && isSymbol(e[y][x+1])) || // check right
-		(!yIsBottom && isSymbol(e[y-1][x])) || // check top
-		(!yIsTop && isSymbol(e[y+1][x])) || // check bottom
-		(!yIsBottom && !xIsBottom && isSymbol(e[y-1][x-1])) || // check top left
-		(!yIsBottom && !xIsTop && isSymbol(e[y-1][x+1])) || // check top right
-		(!yIsTop && !xIsTop && isSymbol(e[y+1][x+1])) || // check bottom right
-		(!yIsTop && !xIsBottom && isSymbol(e[y+1][x-1])) // check bottom left
-}
-
-func part1(engine Engine, visual bool) int {
-	sum := 0
-
-	for y, line := range engine {
-		buf := ""
-		hasSymbol := false
-
-		for x, c := range line {
-			if runes.IsDigit(c) {
-
-				if hasSymbolNeighbours(engine, x, y) {
-					hasSymbol = true
-				}
-
-				buf += string(c)
-
-			} else {
-
-				if len(buf) != 0 && hasSymbol {
-					v, err := strings.Atoi(buf)
-					if err != nil {
-						panic(err)
+			parseBuf := func() {
+				if buf != "" {
+					p := Part{
+						Positions: bufPositions,
+						value:     buf,
+						IsSymbol:  false,
+						IsNumber:  true,
 					}
+					e.Parts = append(e.Parts, p)
 
-					if visual {
-						fmt.Print(colorGreen + buf + colorReset)
-					}
-
-					sum += v
-				} else if visual && len(buf) > 0 && !hasSymbol {
-					fmt.Print(colorRed + buf + colorReset)
+					buf = ""
+					bufPositions = []math.Vector2[int]{}
 				}
-
-				if visual {
-					fmt.Print(string(c))
-				}
-
-				buf = ""
-				hasSymbol = false
 			}
 
+			for x, c := range line {
+				if runes.IsDigit(c) {
+					buf += string(c)
+					bufPositions = append(bufPositions, vec(x, y))
+				} else {
+					parseBuf()
+
+					p := Part{
+						Positions: []math.Vector2[int]{{X: x, Y: y}},
+						value:     string(c),
+						IsSymbol:  isSymbol(c),
+						IsNumber:  false,
+					}
+					e.Parts = append(e.Parts, p)
+				}
+			}
+			parseBuf()
 		}
-		if len(buf) != 0 && hasSymbol {
-			v, err := strings.Atoi(buf)
+
+		return e
+	})
+}
+
+func hasSymbolNeighbours(e Engine, p Part) bool {
+	for _, n := range e.neighboursOf(p) {
+		if n.IsSymbol {
+			return true
+		}
+	}
+
+	return false
+}
+
+func part1(engine Engine) int {
+	sum := 0
+
+	for _, p := range engine.Parts {
+		if p.IsNumber && hasSymbolNeighbours(engine, p) {
+			v, err := strings.Atoi(p.value)
 			if err != nil {
 				panic(err)
 			}
 			sum += v
-		}
-
-		if visual {
-			fmt.Println()
 		}
 	}
 
 	return sum
 }
 
-func part2() int {
-	return -1
+func getNumberNeighbours(e Engine, p Part) []Part {
+	nums := []Part{}
+
+	for _, n := range e.neighboursOf(p) {
+		if n.IsNumber {
+			nums = append(nums, n)
+		}
+	}
+
+	return nums
+}
+
+func part2(engine Engine) int {
+	sum := 0
+
+	for _, p := range engine.Parts {
+		if p.value != "*" {
+			continue
+		}
+
+		numNeighbours := getNumberNeighbours(engine, p)
+		if len(numNeighbours) != 2 {
+			continue
+		}
+
+		v1, err := strings.Atoi(numNeighbours[0].value)
+		if err != nil {
+			panic(err)
+		}
+		v2, err := strings.Atoi(numNeighbours[1].value)
+		if err != nil {
+			panic(err)
+		}
+
+		sum += v1 * v2
+	}
+
+	return sum
 }
 
 func main() {
 	in := getInput()
 
-	fmt.Printf("PART 1: %d\n", part1(in, false))
+	fmt.Printf("PART 1: %d\n", part1(in))
 
 	fmt.Printf("PART 2: %d\n", part2(in))
 }
