b8ec5fab6ace647556fb98f4332cbdfa497a0f29
Tch1b0 <Jpour3006@gmail.com>
2023-12-04 09:35:25 +0100

    solve day 4

--- days/04/solution.go
+++ days/04/solution.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
 	stdstrings "strings"
 
-	"github.com/Tch1b0/polaris/input"
 	"github.com/Tch1b0/polaris/array"
+	"github.com/Tch1b0/polaris/input"
+	"github.com/Tch1b0/polaris/strings"
 )
 
 type Card struct {
-	Got []int
+	Id   int
+	Got  []int
 	Want []int
+}
+
+func (c Card) CountCorrect() int {
+	count := 0
+	for _, n := range c.Got {
+		if array.GetFirst(c.Want, func(v int, _ int) bool { return v == n }) != nil {
+			count++
+		}
+	}
+	return count
 }
 
 func spacedNumsToArr(spacedNums string) []int {
@@ -18,7 +31,9 @@ func spacedNumsToArr(spacedNums string) []int {
 
 	for _, n := range stdstrings.Split(spacedNums, " ") {
 		v, err := strings.Atoi(n)
-		if err != nil { panic(err) }
+		if err != nil {
+			continue
+		}
 		nums = append(nums, v)
 	}
 
@@ -30,40 +45,50 @@ func getInput() []Card {
 		lines := stdstrings.Split(stdstrings.ReplaceAll(str, "\r", ""), "\n")
 		cards := []Card{}
 
-		for _, line := lines {
+		for i, line := range lines {
 			x := stdstrings.Split(line, ":")[1]
 			spacedNums := stdstrings.Split(x, "|")
 			left := spacedNumsToArr(spacedNums[0])
 			right := spacedNumsToArr(spacedNums[1])
-		
-			cards = append(cards, Card{Got: right, Want: left})
+
+			cards = append(cards, Card{Id: i + 1, Got: right, Want: left})
 		}
 
 		return cards
-	}
+	})
 }
 
 func part1(cards []Card) int {
 	sum := 0
 
 	for _, card := range cards {
-		exp := -1
-		for _, n := range card.Got {
-			if array.Contains(card.Want, n) {
-				exp++
-			}
-		}
+		exp := card.CountCorrect() - 1
 
 		if exp > -1 {
-			sum += math.Pow(2, exp)
+			sum += int(math.Pow(2, float64(exp)))
 		}
 	}
 
 	return sum
 }
 
-func part2() int {
-	return -1
+func part2(cards []Card) int {
+	sum := 0
+
+	for i, card := range cards {
+		count := card.CountCorrect()
+
+		// iterate backwards, because forwards loop would be more complex
+		for j := count; j > 0; j-- {
+			// add the count of the copies and its copies
+			sum += part2(cards[i+j : i+j+1])
+		}
+
+		// count this card
+		sum++
+	}
+
+	return sum
 }
 
 func main() {
@@ -71,5 +96,5 @@ func main() {
 
 	fmt.Printf("PART 1: %d\n", part1(in))
 
-	fmt.Printf("PART 2: %d\n", part2())
+	fmt.Printf("PART 2: %d\n", part2(in))
 }
