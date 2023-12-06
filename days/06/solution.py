865f6e3c799064ff92d4484da01f36165aebe8ad
tch1b0 <jpour3006@gmail.com>
2023-12-06 07:55:02 +0100

    solve part 2

--- days/06/solution.py
+++ days/06/solution.py
@@ -6,33 +6,24 @@ with open("days/06/input.txt", "r") as f:
 a, b = content
 
 def parse_nums(s: str):
-    nums = []
-    buf = ""
-    for c in s:
-        if c == " " and len(buf) != 0:
-            nums.append(int(buf))
-            buf = ""
-        elif c.isdigit():
-            buf += c
-    if len(buf) != 0:
-        nums.append(int(buf))
-    return nums
+    return int(s.replace(" ","").replace("\n", "").replace("Time:", "").replace("Distance:", ""))
 
 def distance_for_time(t: int, T: int) -> int:
     return (T - t) * t
     
 
-a, b = parse_nums(a), parse_nums(b)
-races = [x for x in zip(a, b)]
+time, distance = parse_nums(a), parse_nums(b)
 
-r = []
-for time, distance in races:
-    m = 0
-    for t in range(time):
-        d = distance_for_time(t, time)
-        if d > distance:
-            m += 1
-    r += [m]
+front = back = 0
 
-print(r)
-print(math.prod(r))
+for t in range(time):
+    if distance_for_time(t, time) > distance:
+        front = t
+        break
+
+for t in range(time, 0, -1):
+    if distance_for_time(t, time) > distance:
+        back = t
+        break
+
+print(back - front + 1)
